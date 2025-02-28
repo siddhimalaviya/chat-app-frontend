bcadfd3f6544b975d0ed62e08fad6426d9e73cb2
siddhimalaviya <104448504+siddhimalaviya@users.noreply.github.com>
2025-02-28 16:55:05 +0530

    fixed build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,4 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 import ChatApp from "@/component/Chat";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
